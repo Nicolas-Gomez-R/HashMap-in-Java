bb1d18f59ca6a54d21fff112dbfedadbdf4ec9ef
Nicolas Gomez R <71398770+Nicolas-Gomez-R@users.noreply.github.com>
2021-08-08 14:14:52 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+
+        HashMap<String, String> capitalCities = new HashMap<> ();
+
+        capitalCities.put("Colombia", "Bogota");
+        capitalCities.put("Venezuela", "Caracas");
+        capitalCities.put("French Guiana", "Cayenne");
+        capitalCities.put("Guiana", "Georgetown");
+        capitalCities.put("Brazil", "Brasilia");
+        capitalCities.put("Chile", "Santiago (Santiago of Chile)");
+        capitalCities.put("Argentina", "Buenos Aires");
+        capitalCities.put("Paraguay", "Asunción");
+        capitalCities.put("Uruguay", "Montevideo");
+        capitalCities.put("Ecuador", "Quito");
+        capitalCities.put("Peru", "Lima");
+        capitalCities.put("Suriname", "Paramaribo");
+        capitalCities.put("Bolivia", "La Paz and Sucre");
+        System.out.println(capitalCities);
+
+
+    }
+}
